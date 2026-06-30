0138cbcfbd8230bf6c0f73ea6ddf64397d2a949a
EddHGH <Duardo.kk@hotmail.com>
2026-06-30 16:59:25 -0300

    ATT Loja 30/06/26 - 16:59

---
+++ Aula20 - Function/Aula20.js
@@ -0,0 +1,14 @@
+//Funções são blocos ou estruturas de comandos que podem ser executados apenas quando chamados no meio do código.
+//Estrutura
+
+function MudarTexto(){ //Método "Function" + nome da função + (parâmetros) + {}
+    let d1 = document.getElementById("d1")
+    d1.innerHTML = "Editado1"
+   
+}
+
+function MudarTexto2(){ //Método "Function" + nome da função + (parâmetros) + {}
+    let d1 = document.getElementById("d1")
+    d1.innerHTML = "Editado2"
+   
+}
