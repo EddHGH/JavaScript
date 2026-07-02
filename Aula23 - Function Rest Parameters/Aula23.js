4041f321c3ca969961e30ef6053578930c8b6230
EddHGH <duardo.kk@hotmail.com>
2026-07-01 22:03:27 -0300

    Att Ufersa - 01-07-2026 - 22:03

--- Aula23 - Function Rest Parameters/Aula23.js
+++ Aula23 - Function Rest Parameters/Aula23.js
@@ -1,5 +1,7 @@
-function soma(n1=0,n2 = 0){
-    return n1 + n2
+
+
+function soma(n1 = 0, n2 = 0) {
+  return n1 + n2;
 }
 
 //console.log(soma(3,5)) // Para funções co parâmetros definidos, perceba que eu não posso usar mais do que duas variáveis aqui.
@@ -7,8 +9,33 @@ function soma(n1=0,n2 = 0){
 
 //Veja abaixo:
 
-function SomaCorreta(...valores){ //Valores é um array com método spread
-    return valores.length //Retornará o tamanho o array valores.
+function SomaCorreta(...valores) {
+  //Valores é um array com método spread
+  return valores.length; //Retornará o tamanho o array valores.
 } //O correto aqui é se usar um FOR e mais correto ainda FOR OF, terminar depois.
 
-console.log(SomaCorreta[5,6,5])
+function SomaCorretaReal(...valores) {
+  let tam = valores.length;
+  let res = 0;
+  for (let i = 0; i < tam; i++) {
+    //O contador está justamente percorrendo o Array valores, e somando os resultados que foram postos no array.
+
+    res += valores[i];
+  }
+
+  return res;
+}
+// console.log(SomaCorretaReal(10,15,30,40))
+
+//Veja agora uma boa prática com o FOR, usando FOR OF
+
+function SomaCorretaReal2(...valores) {
+  let tam = valores.length;
+  let res = 0;
+  for (let v of valores) { //Construção mais simples com FOR OF e retornando mesmo resultado.
+    res += v;
+  }
+
+  return res;
+}
+console.log(SomaCorretaReal2(10,5,13,2,4,3,3))
